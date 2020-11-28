e459808b9cba5dff8c478e206795ab6373a9154e
roofWalker <roofwalker13@outlook.com>
2020-11-27 23:26:49 -0600

    Forms

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -3,12 +3,18 @@ import { Text, View, ScrollView, FlatList } from 'react-native';
 import { Card, Icon } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { postFavorite } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return {
         campsites: state.campsites,
-        comments: state.comments
+        comments: state.comments,
+        favorites: state.favorites
     };
+};
+
+const mapDispatchToProps = {
+    postFavorite: campsiteId => (postFavorite(campsiteId))
 };
 
 function RenderCampsite(props) {
@@ -18,18 +24,18 @@ function RenderCampsite(props) {
     if (campsite) {
         return (
             <Card
-                featuredTitle={campsite.name}
-                image={{uri: baseUrl + campsite.image}}>
-                <Text style={{margin: 10}}>
+                featuredTitle = {campsite.name}
+                image = {{uri: baseUrl + campsite.image}}>
+                <Text style = {{margin: 10}}>
                     {campsite.description}
                 </Text>
                 <Icon
-                    name={props.favorite ? 'heart' : 'heart-o'}
-                    type='font-awesome'
-                    color='#f50'
+                    name = {props.favorite ? 'heart' : 'heart-o'}
+                    type = 'font-awesome'
+                    color = '#f50'
                     raised
                     reverse
-                    onPress={() => props.favorite ? 
+                    onPress = {() => props.favorite ? 
                         console.log('Already set as a favorite') : props.markFavorite()}
                 />
             </Card>
@@ -43,19 +49,19 @@ function RenderComments({comments}) {
     const renderCommentItem = ({item}) => {
         return (
             <View style={{margin: 10}}>
-                <Text style={{fontSize: 14}}>{item.text}</Text>
-                <Text style={{fontSize: 12}}>{item.rating} Stars</Text>
-                <Text style={{fontSize: 12}}>{`-- ${item.author}, ${item.date}`}</Text>
+                <Text style = {{fontSize: 14}}>{item.text}</Text>
+                <Text style = {{fontSize: 12}}>{item.rating} Stars</Text>
+                <Text style = {{fontSize: 12}}>{`-- ${item.author}, ${item.date}`}</Text>
             </View>
         );
     };
 
     return (
-        <Card title='Comments'>
+        <Card title = 'Comments'>
             <FlatList
-                data={comments}
-                renderItem={renderCommentItem}
-                keyExtractor={item => item.id.toString()}
+                data = {comments}
+                renderItem = {renderCommentItem}
+                keyExtractor = {item => item.id.toString()}
             />
         </Card>
     );
@@ -63,22 +69,9 @@ function RenderComments({comments}) {
 
 class CampsiteInfo extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            favorite: false
-        };
+    markFavorite(campsiteId) {
+        this.props.postFavorite(campsiteId);
     }
-
-
-    markFavorite() {
-        this.setState({favorite: true});
-    }
-
-
-
-
-
     static navigationOptions = {
         title: 'Campsite Information'
     }
@@ -89,14 +82,14 @@ class CampsiteInfo extends Component {
         const comments = this.props.comments.comments.filter(comment => comment.campsiteId === campsiteId);
         return (
             <ScrollView>
-                <RenderCampsite campsite={campsite}
-                    favorite={this.state.favorite}
-                    markFavorite={() => this.markFavorite()}
+                <RenderCampsite campsite = {campsite}
+                    favorite = {this.props.favorites.includes(campsiteId)}
+                    markFavorite = {() => this.markFavorite(campsiteId)}
                 />
-                <RenderComments comments={comments} />
+                <RenderComments comments = {comments} />
             </ScrollView>
         );
     }
 }
 
-export default connect(mapStateToProps)(CampsiteInfo);
+export default connect(mapStateToProps, mapDispatchToProps)(CampsiteInfo);
