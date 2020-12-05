eabb716928913a429e011c15e9cf648b1ff04f13
roofWalker <roofwalker13@outlook.com>
2020-12-05 11:15:36 -0600

    Animatable

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -4,6 +4,7 @@ import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import Loading from './LoadingComponent';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
     return {
@@ -14,7 +15,7 @@ const mapStateToProps = state => {
 function RenderMission() {
     return (
         <Card title='Our Mission'>
-            <Text>
+            <Text style = {{margin: 10}}>
                 We present a curated database of the best campsites in the vast woods and back-country of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share review on campsites they have visited with each other.
             </Text>
         </Card>
@@ -51,24 +52,37 @@ class About extends Component {
         if (this.props.partners.errMess) {
             return (
                 <ScrollView>
-                    <RenderMission />
-                    <Card
-                        title ='Community Partners'>
-                        <Text>{this.props.partners.errMess}</Text>
-                    </Card>
+                    <Animatable.View
+                        animation = 'fadeInDown'
+                        duration = {2000}
+                        delay = {1000}
+                    >
+                        <RenderMission />
+                        <Card
+                            title = "Community Partners">
+                            <Text> {this.props.partners.errMess} </Text>
+                        </Card>
+                    </Animatable.View>
                 </ScrollView>
             );
         }
         return (
             <ScrollView>
-                <RenderMission />
-                <Card title = "Community Partners">
-                    <FlatList
-                    data = {this.props.partners.partners}
-                    renderItem = {renderPartner}
-                    keyExtractor = {item => item.id.toString()}
-                    />
-                </Card>
+                <Animatable.View
+                    animation = 'fadeInDown'
+                    duration = {2000}
+                    delay = {1000}
+                >
+                    <RenderMission />
+                    <Card
+                        title = "Community Partners">
+                        <FlatList
+                            data = {this.props.partners.partners}
+                            renderItem = {renderPartner}
+                            keyExtractor = {item => item.id.toString()}
+                        />
+                    </Card>
+                </Animatable.View>
             </ScrollView>
         );
     }
